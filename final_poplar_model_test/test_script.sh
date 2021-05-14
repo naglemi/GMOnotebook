14c3974bc55cbde9f6b567e8424f491eb67aac4e
naglemi <michael.nagle@oregonstate.edu>
2021-05-14 13:46:10 -0700

    added final poplar model

---
+++ final_poplar_model_test/test_script.sh
@@ -0,0 +1,2 @@
+jupyter nbconvert --to HTML --ExecutePreprocessor.timeout=-1 --allow-errors --execute GRF_phase1_t1_v0.2.08_postimagerfix.ipynb
+jupyter nbconvert --to HTML --ExecutePreprocessor.timeout=-1 --allow-errors --execute GRF_phase3_t1_v0.2.08_postimagerfix.ipynb
