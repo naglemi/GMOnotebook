023ad65005781fc7f4f6d05f770d904aa3f45423
Michael Nagle <michael.nagle@oregonstate.edu>
2022-10-16 21:32:35 -0700

    Clean-up: Removed old, unimportant scripts that have been archived

--- .ipynb_checkpoints/test_script-checkpoint.sh
+++
@@ -1,2 +0,0 @@
-jupyter nbconvert --to HTML --ExecutePreprocessor.timeout=-1 --allow-errors --execute GRF_phase1_t1_v0.2.08_postimagerfix.ipynb
-jupyter nbconvert --to HTML --ExecutePreprocessor.timeout=-1 --allow-errors --execute GRF_phase3_t1_v0.2.08_postimagerfix.ipynb
